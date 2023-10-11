67f390b726b3b537bc1142cf7f0c77150da48743
Matteo Bonfadini <teobonfa2000@gmail.com>
2023-10-11 14:53:41 +0200

    time

--- Lab1/sudoku_initial.cpp
+++ Lab1/sudoku_initial.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <cmath>
+#include <chrono>
+#include <unistd.h>
 
 using std::cout;
 using std::cin;
@@ -24,23 +26,26 @@ int basic_search (const unsigned v[], unsigned n_elements);
    (respectively) of the Sudoku matrix comply with the rules, 0 otherwise */
 int check_rows (const unsigned sudoku[][SIZE]);
 int check_cols (const unsigned sudoku[][SIZE]);
-int check_regions (const unsigned sudoku[][SIZE]);
+int check_regions_M (const unsigned sudoku[][SIZE]);
+int check_regions_L (const unsigned sudoku[][SIZE]);
+int check_regions_C (const unsigned sudoku[][SIZE]);
+int check_regions_D (const unsigned sudoku[][SIZE]);
 
 /* Function that returns:
          1 if sudoku matrix complies to all Sudoku rules
         -1 if a row violates the game rules
         -2 if a column violates the game rules
         -3 if a region violates the game rules */
-int check_sudoku(const unsigned sudoku[][SIZE]);
+int check_sudoku (const unsigned sudoku[][SIZE], unsigned choose);
 
 /* Function that create a Sudoku matrix by Lewis' Algorithm
    (https://en.wikipedia.org/wiki/Sudoku_solving_algorithms) */
-void generate_sudoku(unsigned sudoku[][SIZE]);
+void generate_sudoku (unsigned sudoku[][SIZE]);
 
 /* Function that shows on output the sudoku matrix */
-void show_sudoku(unsigned sudoku[][SIZE]);
+void show_sudoku (unsigned sudoku[][SIZE]);
 
-int main()
+int main ()
 {
     // initialize a sudoku matrix
     unsigned sudoku[SIZE][SIZE] = {
@@ -59,7 +64,7 @@ int main()
     cout << "\n";
 
     // check
-    int res = check_sudoku(sudoku);
+    int res = check_sudoku(sudoku,1);
     cout << "check_sudoku returns: " <<  res << "\n\n\n";
 
     cout << "\n";
@@ -72,7 +77,10 @@ int main()
     cout << "\n";
 
     // check
-    res = check_sudoku(sudoku2);
+    res = check_sudoku(sudoku2,1);
+    res = check_sudoku(sudoku2,2);
+    res = check_sudoku(sudoku2,3);
+    res = check_sudoku(sudoku2,4);
     cout << "check_sudoku returns: " <<  res << endl;
 
     return 0;
@@ -143,7 +151,10 @@ int check_cols (const unsigned sudoku[][SIZE])
 }
 
 // Matteo's version
-int check_regions (const unsigned sudoku[][SIZE]) {
+int check_regions_M (const unsigned sudoku[][SIZE])
+{
+    auto start = std::chrono::steady_clock::now();
+
     unsigned S[SIZE];
 
     for (size_t k = 0; k < 3; ++k)
@@ -162,34 +173,53 @@ int check_regions (const unsigned sudoku[][SIZE]) {
             }
         }
     }
+    auto end = std::chrono::steady_clock::now();
+    cout << "Elapsed time in nanoseconds: "
+         << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
+         << " ns (Matteo)" << endl;
+
     return 1;
 }
 
-/* // Lorenzo's version
-int check_regions (const unsigned sudoku[][SIZE]){
-    int flag=1;
-    unsigned v[SIZE]={};
-    unsigned t=0;
+// Lorenzo's version
+int check_regions_L (const unsigned sudoku[][SIZE])
+{
+    auto start = std::chrono::steady_clock::now();
 
-    for (size_t i=0; i<3; i++) {
-        for (size_t j=0; j<3; j++) {
-            for(size_t ii=3*i; ii<3*(i+1); ii++)  {
-                for (size_t jj = 3*j; jj<3*(j+1); jj++) {
-                    v[t] = sudoku[ii][jj];
-                    t++;
+    unsigned v[SIZE] = {};
+
+    for (size_t i=0; i<3; ++i)
+    {
+        for (size_t j=0; j<3; ++j)
+        {
+            unsigned t = 0;
+
+            for(size_t ii=3*i; ii<3*(i+1); ++ii)
+            {
+                for (size_t jj = 3*j; jj<3*(j+1); ++jj)
+                {
+                    v[t++] = sudoku[ii][jj];
                 }
             }
             if (basic_search(v,SIZE)==0)
-                flag=0;
-            t=0;
+            {
+                return 0;
+            }
         }
     }
-    return flag;
-}*/
+    auto end = std::chrono::steady_clock::now();
+    cout << "Elapsed time in nanoseconds: "
+         << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
+         << " ns (Lorenzo)" << endl;
 
-/* // Carlotta's version
-int check_regions(const unsigned sudoku[][SIZE])
+    return 1;
+}
+
+// Carlotta's version
+int check_regions_C (const unsigned sudoku[][SIZE])
 {
+    auto start = std::chrono::steady_clock::now();
+
     for (size_t i=0; i<SIZE-3; i=i+3)
     {
         for (size_t j=0; j<SIZE-3; j=j+3)
@@ -208,13 +238,19 @@ int check_regions(const unsigned sudoku[][SIZE])
                 return 0;
         }
     }
+    auto end = std::chrono::steady_clock::now();
+    cout << "Elapsed time in nanoseconds: "
+         << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
+         << " ns (Carlotta)" << endl;
+
     return 1;
 }
-*/
 
-/* // Davide's version
-int check_regions(const unsigned sudoku[][SIZE])
+// Davide's version
+int check_regions_D (const unsigned sudoku[][SIZE])
 {
+    auto start = std::chrono::steady_clock::now();
+
     unsigned v[SIZE] = {};
 
     for (size_t region = 0; region < SIZE; ++region)
@@ -233,31 +269,44 @@ int check_regions(const unsigned sudoku[][SIZE])
             return 0;
         }
     }
-    return 1;
-}*/
+    auto end = std::chrono::steady_clock::now();
+    cout << "Elapsed time in nanoseconds: "
+         << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
+         << " ns (Davide)" << endl;
 
-int check_sudoku(const unsigned sudoku[][SIZE])
+    return 1;
+}
+
+int check_sudoku (const unsigned sudoku[][SIZE], unsigned choose)
 {
     if (check_rows(sudoku)==0)
     {
-        // cout << "Hei pal, rows are incorrect" << endl;
         return -1;
     }
     if (check_cols(sudoku)==0)
     {
-        // cout << "Hei pal, cols are incorrect" << endl;
         return -2;
     }
-    if (check_regions(sudoku)==0)
+    if (choose == 1 && check_regions_M(sudoku)==0)
     {
-        // cout << "Hei pal, 3x3 subregions are incorrect" << endl;
         return -3;
     }
-    // cout << "Hei pal, everything is fine with this sudoku. You can start it!" << endl;
+    else if (choose == 2 && check_regions_L(sudoku)==0)
+    {
+        return -3;
+    }
+    else if (choose == 3 && check_regions_C(sudoku)==0)
+    {
+        return -3;
+    }
+    else if (choose == 4 && check_regions_D(sudoku)==0)
+    {
+        return -3;
+    }
     return 1;
 }
 
-void generate_sudoku(unsigned sudoku[][SIZE])
+void generate_sudoku (unsigned sudoku[][SIZE])
 {
     int x = 0;
 
@@ -276,7 +325,7 @@ void generate_sudoku(unsigned sudoku[][SIZE])
     }
 }
 
-void show_sudoku(unsigned sudoku[][SIZE])
+void show_sudoku (unsigned sudoku[][SIZE])
 {
     for (size_t r = 0; r<SIZE; r++)
     {
